a05a64ea0f809367a9cfb476445b8ddb45aa6926
lennox02 <jameswebbdev@gmail.com>
2017-09-18 10:14:40 -0700

    dates_when_user_was_in_top_n()
    --Wrote sql for identifying dates when user was in top range of scores.
    --Fixed identantion in some areas
    --added correct params for several codeblocks

--- solution.php
+++ solution.php
@@ -1,11 +1,10 @@
 <?php
-
 /**
  * Solution functions
  *
  * Functions that allow the unit tests to parse an incoming request, identify
  * dates with a specific number of scores, identify users with highscore by
- * date, and identify dates when a user was in a top percentile
+ * date, and identify dates when a user was in a top range
  *
  * PHP version 5
  *
@@ -49,7 +48,7 @@ function parse_request($request, $secret)
  *
  * @param $pdo
  * @param $n
- * @return
+ * @return array
  */
 function dates_with_at_least_n_scores($pdo, $n)
 {
@@ -72,21 +71,21 @@ function dates_with_at_least_n_scores($pdo, $n)
 /**
  * List of users with top score on date
  *
- * @param $request
- * @param $secret
- * @return
+ * @param $pdo
+ * @param $date
+ * @return array
  */
 function users_with_top_score_on_date($pdo, $date)
 {
     $sql = "
-      SELECT user_id
-      FROM scores
-      WHERE date = :date AND score = (
-        SELECT MAX(score)
+        SELECT user_id
         FROM scores
-        WHERE date = :date
-        GROUP BY date
-      )
+        WHERE date = :date AND score = (
+            SELECT MAX(score)
+            FROM scores
+            WHERE date = :date
+            GROUP BY date
+        )
     ";
 
     $query = $pdo->prepare($sql);
@@ -98,13 +97,34 @@ function users_with_top_score_on_date($pdo, $date)
 
 
 /**
- * List of dates when user was in specified top percentile
+ * List of dates when user was in specified top range of results
  *
- * @param $request
- * @param $secret
- * @return
+ * @param $pdo
+ * @param $user_id
+ * @param $n
+ * @return array
  */
 function dates_when_user_was_in_top_n($pdo, $user_id, $n)
 {
-    // YOUR CODE GOES HERE
+    //use date alias to see if user's score is in the top n scores where the
+    //date matches the alias (allows us to loop through each date)
+    $sql = "
+        SELECT date as dateAlias
+        FROM scores
+        WHERE user_id = :user_id
+        AND score IN (
+            SELECT score
+            FROM scores
+            WHERE date = dateAlias
+            ORDER BY score DESC
+            LIMIT :n
+        )
+        ORDER BY dateAlias DESC
+    ";
+    $query = $pdo->prepare($sql);
+    $query->bindParam(':user_id', $user_id, PDO::PARAM_INT); //bind param as int only
+    $query->bindParam(':n', $n, PDO::PARAM_INT); //bind param as int only
+    $query->execute();
+
+    return $query->fetchAll(PDO::FETCH_COLUMN);
 }
